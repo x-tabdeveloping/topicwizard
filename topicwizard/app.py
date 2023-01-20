ac05b9260faca6ae8fa17a40b7d00ffa8d042eeb
Márton Kardos <power.up1163@gmail.com>
2023-01-20 15:31:38 +0100

    Modified is_notebook

--- topicwizard/app.py
+++ topicwizard/app.py
@@ -13,17 +13,21 @@ from topicwizard.blueprints.template import prepare_blueprint
 from topicwizard.blueprints.app import create_blueprint
 
 
-def is_notebook():
+def is_notebook() -> bool:
     try:
         from IPython import get_ipython
 
-        if "IPKernelApp" not in get_ipython().config:  # type: ignore
-            return False
-    except ImportError:
-        return False
-    except AttributeError:
-        return False
-    return True
+        shell = get_ipython().__class__.__name__
+        if shell == "ZMQInteractiveShell":
+            return True  # Jupyter notebook or qtconsole
+        elif shell == "TerminalInteractiveShell":
+            return False  # Terminal running IPython
+        else:
+            return True  # Other type (?)
+    except NameError:
+        return False  # Probably standard Python interpreter
+    except ModuleNotFoundError:
+        return False  # Probably standard Python interpreter
 
 
 def get_app_blueprint(

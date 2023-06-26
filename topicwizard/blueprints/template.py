6a2bf896f9b5654624b2bf455e59d76430aa8eba
Márton Kardos <power.up1163@gmail.com>
2023-06-26 09:37:43 +0200

    Filtering nans in preprocessing

--- topicwizard/blueprints/template.py
+++ topicwizard/blueprints/template.py
@@ -1,5 +1,7 @@
-from typing import Any, Iterable, Callable, List, Optional
+from typing import Any, Callable, Iterable, List, Optional
+from warnings import warn
 
+import numpy as np
 from dash_extensions.enrich import DashBlueprint
 
 from topicwizard.prepare.utils import get_vocab, prepare_transformed_data
@@ -25,6 +27,17 @@ def prepare_blueprint(
         document_topic_matrix,
         topic_term_matrix,
     ) = prepare_transformed_data(vectorizer, topic_model, corpus)
+    nan_documents = np.isnan(document_topic_matrix).any(axis=1)
+    n_nan_docs = np.sum(nan_documents)
+    if n_nan_docs:
+        warn(
+            f"{n_nan_docs} documents had nan values in the output of the topic model,"
+            " these are removed in preprocessing and will not be visible in the app."
+        )
+        corpus = list(np.array(corpus)[~nan_documents])
+        document_topic_matrix = document_topic_matrix[~nan_documents]
+        document_term_matrix = document_term_matrix[~nan_documents]
+        document_names = list(np.array(document_names)[~nan_documents])
     n_topics = topic_term_matrix.shape[0]
     if topic_names is None:
         topic_names = [f"Topic {i}" for i in range(n_topics)]

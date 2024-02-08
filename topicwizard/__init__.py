8c481958b31a48cadebfb84936c76cae165a8550
Márton Kardos <power.up1163@gmail.com>
2024-02-08 13:51:44 +0100

    Compatibility layers now have to be imported from topicwizard.compatibility

--- topicwizard/__init__.py
+++ topicwizard/__init__.py
@@ -1,6 +1,4 @@
 from topicwizard.app import get_dash_app, load, load_app, visualize
-from topicwizard.compatibility.bertopic import bertopic_pipeline
-from topicwizard.compatibility.gensim import gensim_pipeline
 from topicwizard.prepare.topics import infer_topic_names
 
 __all__ = [
@@ -9,6 +7,4 @@ __all__ = [
     "load_app",
     "load",
     "infer_topic_names",
-    "gensim_pipeline",
-    "bertopic_pipeline",
 ]
--- topicwizard/compatibility/__init__.py
+++ topicwizard/compatibility/__init__.py
@@ -1,3 +1,4 @@
+from topicwizard.compatibility.bertopic import BERTopicWrapper
 from topicwizard.compatibility.gensim import gensim_pipeline
 
-__all__ = ["gensim_pipeline"]
+__all__ = ["gensim_pipeline", "BERTopicWrapper"]

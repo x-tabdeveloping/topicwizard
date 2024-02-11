f61ad970d6cd79d467c3329025c7afb4bd659efc
Márton Kardos <power.up1163@gmail.com>
2024-02-11 11:25:58 +0100

    Added tests for compatibility layers

---
+++ tests/test_compatibility.py
@@ -0,0 +1,37 @@
+from bertopic import BERTopic
+from gensim.corpora.dictionary import Dictionary
+from gensim.models import LdaModel
+from gensim.utils import tokenize
+from sklearn.datasets import fetch_20newsgroups
+
+import topicwizard
+from topicwizard.compatibility import BERTopicWrapper, gensim_pipeline
+
+newsgroups = fetch_20newsgroups(
+    subset="all",
+    categories=[
+        "misc.forsale",
+        "sci.med",
+        "comp.graphics",
+        "alt.atheism",
+        "talk.politics.misc",
+    ],
+    remove=("headers", "footers", "quotes"),
+)
+corpus = newsgroups.data
+
+
+def test_bertopic():
+    model = BERTopicWrapper(BERTopic(language="english"))
+    topic_data = model.prepare_topic_data(corpus)
+    app = topicwizard.get_dash_app(topic_data)
+
+
+def test_gensim():
+    tokenized_corpus = [list(tokenize(text, lower=True)) for text in corpus]
+    dictionary = Dictionary(tokenized_corpus)
+    bow_corpus = [dictionary.doc2bow(text) for text in tokenized_corpus]
+    lda = LdaModel(bow_corpus, num_topics=10)
+    pipeline = gensim_pipeline(dictionary, model=lda)
+    topic_data = pipeline.prepare_topic_data(corpus)
+    app = topicwizard.get_dash_app(topic_data)

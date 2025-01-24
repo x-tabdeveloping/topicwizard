646f2c7d8a23216c4a621c1f3c70a9157a702041
Márton Kardos <power.up1163@gmail.com>
2025-01-24 13:49:17 +0100

    Switched to tabs from SegmentedControl

--- topicwizard/widgets/widget_container.py
+++ topicwizard/widgets/widget_container.py
@@ -5,6 +5,7 @@ from typing import Callable, Dict, List, Optional, Set
 import dash_mantine_components as dmc
 from dash_extensions.enrich import (DashBlueprint, Input, Output, State, dcc,
                                     exceptions, html)
+from dash_iconify import DashIconify
 
 from topicwizard.data import TopicData
 
@@ -16,8 +17,9 @@ def create_widget_container(
 ) -> DashBlueprint:
     # --------[ Collecting blueprints ]--------
     ids = []
+    names = []
+    icons = []
     blueprints = []
-    options = []
     for widget in widgets:
         try:
             widget.validate_data(topic_data)
@@ -25,58 +27,44 @@ def create_widget_container(
             warnings.warn(f"Widget {widget.name} could not be created. Reason: {e}")
         blueprints.append(widget.create_blueprint(topic_data, app_id=app_id))
         component_id = f"{widget.id_prefix}_{app_id}"
-        options.append({"value": component_id, "label": widget.name})
         ids.append(component_id)
+        icons.append(widget.icon)
+        names.append(widget.name)
 
     # --------[ Creating app blueprint ]--------
     app_blueprint = DashBlueprint()
 
     app_blueprint.layout = html.Div(
-        [
-            html.Div(
-                [
-                    dmc.SegmentedControl(
-                        id=f"page_picker_{app_id}",
-                        data=options,
-                        value=ids[0],
-                        color="blue",
-                        size="md",
-                        radius="sm",
-                        mb=10,
-                        fullWidth=True,
-                    ),
+        dmc.Tabs(
+            [
+                dmc.TabsList(
+                    [
+                        dmc.Tab(name, icon=DashIconify(icon=icon), value=component_id)
+                        for name, icon, component_id in zip(names, icons, ids)
+                    ],
+                    position="center",
+                ),
+                *[
+                    dmc.TabsPanel(blueprint.layout, value=component_id)
+                    for blueprint, component_id in zip(blueprints, ids)
                 ],
-                className="top-0 left-0 flex-none",
-            ),
-            html.Div(
-                [blueprint.layout for blueprint in blueprints],
-                className="flex flex-col flex-1",
-            ),
-        ],
+            ],
+            color="blue",
+            orientation="horizontal",
+            className="w-full h-full flex-1 overflow-y-scroll items-stretch flex flex-col p-8",
+            value=ids[0],
+            styles={
+                "panel": {
+                    "flex-grow": "1",
+                    "flex-direction": "column",
+                    "height": "100%",
+                }
+            },
+        ),
         className="""
-            w-full h-full flex-col flex items-stretch overflow-y-scroll
+            w-full h-full flex-col flex items-stretch
             bg-white
         """,
     )
-
-    # --------[ Registering callbacks ]--------
-    for blueprint in blueprints:
-        blueprint.register_callbacks(app_blueprint)
-
-    @app_blueprint.callback(
-        *[Output(component_id, "className") for component_id in ids],
-        Input(f"page_picker_{app_id}", "value"),
-        prevent_initial_callback=False,
-    )
-    def switch_page(component_id: str):
-        hidden = "hidden"
-        visible = "flex flex-1 flex-col p-3"
-        res = [hidden] * len(ids)
-        for i_component in range(len(ids)):
-            if component_id == ids[i_component]:
-                res[i_component] = visible
-                break
-        print(res)
-        return res
 
     return app_blueprint

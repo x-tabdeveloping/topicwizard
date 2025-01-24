867a51d8858d8931b25feaf08bf8d73572ea5d90
Márton Kardos <power.up1163@gmail.com>
2025-01-24 09:33:23 +0100

    Added word_clusters widget

---
+++ topicwizard/widgets/word_clusters.py
@@ -0,0 +1,126 @@
+from typing import List, Optional
+
+import dash_mantine_components as dmc
+import numpy as np
+import pandas as pd
+import plotly.express as px
+from dash_extensions.enrich import (DashBlueprint, Input, Output, State, dcc,
+                                    html)
+from matplotlib.colors import ListedColormap
+
+import topicwizard.prepare.words as prepare
+
+
+def produce_map(
+    topic_term_matrix: np.ndarray,
+    vocab: np.ndarray,
+    word_positions: np.ndarray,
+    topic_names: List[str],
+):
+    word_topic_assignment = np.argmax(topic_term_matrix, axis=0)
+    word_topic_assignment = [topic_names[i_topic] for i_topic in word_topic_assignment]
+    word_importance = np.max(topic_term_matrix, axis=0)
+    top_min = np.min(word_importance[np.argsort(-word_importance)[:150]])
+    show_text = np.where(word_importance > top_min, vocab, "")
+    word_df = pd.DataFrame(
+        dict(
+            x=word_positions[0],
+            y=word_positions[1],
+            topic=word_topic_assignment,
+            show_text=show_text,
+            word=vocab,
+            importance=word_importance,
+        )
+    )
+    fig = px.scatter(
+        word_df,
+        x="x",
+        y="y",
+        text="show_text",
+        color="topic",
+        size="importance",
+        template="plotly_white",
+        category_orders={"topic": topic_names},
+        hover_name="word",
+        hover_data={
+            "importance": True,
+            "show_text": False,
+            "topic": True,
+            "x": False,
+            "y": False,
+        },
+        color_discrete_sequence=px.colors.qualitative.Dark24,
+        size_max=60,
+    )
+    text_size = (np.sqrt(word_importance) / np.sqrt(np.max(word_importance))) * 22 + 8
+    fig.update_traces(textfont=dict(size=text_size))
+    fig.update_layout(
+        margin=dict(
+            l=0,
+            r=0,
+            t=0,
+            b=0,
+        ),
+        legend=dict(
+            yanchor="bottom",
+            y=0,
+            xanchor="right",
+            x=1,
+            bgcolor="rgba(256, 256, 256, 0.4)",
+            bordercolor="black",
+            borderwidth=2,
+        ),
+        hoverlabel=dict(
+            bgcolor="white",
+            font_size=16,
+        ),
+        legend_title_text="Topic",
+        dragmode="pan",
+        font=dict(
+            color="black",
+            size=16,
+        ),
+    )
+    fig.update_xaxes(title="", showticklabels=False)
+    fig.update_yaxes(title="", showticklabels=False)
+    return fig
+
+
+def create_word_clusters(
+    vocab: np.ndarray,
+    corpus: List[str],
+    topic_term_matrix: np.ndarray,
+    topic_names: List[str],
+    word_positions: Optional[np.ndarray] = None,
+    **kwargs,
+) -> DashBlueprint:
+    # --------[ Preparing data ]--------
+    if word_positions is None:
+        word_positions = prepare.word_positions(topic_term_matrix=topic_term_matrix)
+    word_map = produce_map(topic_term_matrix, vocab, word_positions, topic_names)
+    # --------[ Creating app blueprint ]--------
+    app_blueprint = DashBlueprint()
+    app_blueprint.layout = html.Div(
+        [
+            dmc.Center(
+                [
+                    dmc.Text(
+                        "Concept Clusters",
+                        size="xl",
+                        ta="center",
+                        fw=700,
+                        className="pb-6",
+                    ),
+                ]
+            ),
+            dcc.Graph(
+                figure=word_map, className="flex-1 flex", config=dict(scrollZoom=True)
+            ),
+        ],
+        className="""
+        flex flex-1 flex-col
+        p-3 h-full
+        """,
+        id="word_clusters",
+    )
+    return app_blueprint
